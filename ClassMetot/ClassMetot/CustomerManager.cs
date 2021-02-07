6fc99f312b40f8c5b34f7fc282cf7347301a3bc5
aysesena-yksl <66917703+aysesena-yksl@users.noreply.github.com>
2021-02-08 00:19:17 +0300

    Kamp 3.Gün Ödev

---
+++ ClassMetot/ClassMetot/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void CustomerAdd(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " " + customer.name + " " + customer.lastname + " " + customer.AccountNumber + "Müşteri Eklendi");
+        }
+
+        public void CustomerDelete(Customer customer)
+        {
+            Console.WriteLine(customer.Id + " " + customer.name + " " + customer.lastname + " " + customer.AccountNumber + "Müşteri Silindi");
+        }
+
+        public void List(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + ": " + customer.name + ": " + customer.lastname + ": " + customer.AccountNumber + ": ");
+
+
+            }
+        }
+    }
+}
---
+++ ClassMetot/ClassMetot/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id = 1, name = "Ayşe Sena", lastname = "Yüksel", AccountNumber = 987654 };
+            Customer customer2 = new Customer() { Id = 2, name = "İremnur", lastname = "Yüksel", AccountNumber = 98765524 };
+            Customer customer3 = new Customer() { Id = 3, name = "Talha ", lastname = "Yüksel", AccountNumber = 127654 };
+
+
+            CustomerManager customerManager = new CustomerManager();
+            Console.WriteLine("*****Müşteriler Listelendi*****");
+            Customer[] customers = new Customer[]
+            { customer1, customer2,customer3};
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.Id + ": " + customer.name + ": " + customer.lastname + ": " + customer.AccountNumber + ": ");
+            }
+
+            Console.WriteLine("*****Eklenen Müşteriler:******");
+            customerManager.CustomerAdd(customer1);
+            customerManager.CustomerAdd(customer2);
+            customerManager.CustomerAdd(customer3);
+
+            Console.WriteLine("*******Silinen Müşteriler:******");
+            customerManager.CustomerDelete(customer1);
+            customerManager.CustomerDelete(customer2);
+            customerManager.CustomerDelete(customer3);
+
+        }
+    }
+}
